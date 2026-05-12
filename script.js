40796cb3ec633e9042ce39b63c53c9142ff46cf6
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-05-12 22:45:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,21 @@
-// Your Script here.
-
-const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
-};
-
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+  let decodedArr = []; 
+  
+  // 1. Split the string into an array of characters
+  // 2. Iterate through each character
+  for (let i = 0; i < encodedStr.length; i++) {
+    let char = encodedStr[i];
 
-  return; //return decodedArr
+    // Check if the character exists in our lookup table
+    if (lookup[char] !== undefined) {
+      // If it exists, push the decoded version to our array
+      decodedArr.push(lookup[char]);
+    } else {
+      // If it's not in the lookup (like a space or '!'), pass it through as is
+      decodedArr.push(char);
+    }
+  }
+
+  // Join the array back into a string and return it
+  return decodedArr.join(""); 
 }
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-// Do not change this line
-window.rot13 = rot13;
